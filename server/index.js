ec35c4fcd361f7df61b504382d6172a5ca128945
https://github.com/its-pankaj15/api-testing-tool <you@example.com>
2026-01-18 12:20:11 +0530

    Add root route

--- server/index.js
+++ server/index.js
@@ -35,3 +35,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+app.get("/", (req, res) => {
+  res.send("API Testing Tool Backend Running ✅");
+});
