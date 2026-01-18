280eb01bebcdec8951f8f77bc5dceca0760d0506
https://github.com/its-pankaj15/api-testing-tool <you@example.com>
2026-01-18 12:31:52 +0530

    Frontend ready for deployment

--- server/index.js
+++ server/index.js
@@ -35,6 +35,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
-app.get("/", (req, res) => {
-  res.send("API Testing Tool Backend Running ✅");
-});
